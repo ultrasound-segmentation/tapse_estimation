1c2b134fdd882c8d273090a249846e0f37749aad
Elin Skånlund Berntsen <154683890+elinsbe@users.noreply.github.com>
2026-09-15 15:49:41 +0200

    chore(#4): create setup.sh (#5)
    * chore(#4): create setup.sh
    
    * fix(#4): remove conda module

---
+++ setup.sh
@@ -0,0 +1,16 @@
+echo "Starting setup script!"
+module load Python/3.12.3-GCCcore-13.3.0
+echo "Loaded Python"
+
+
+if [[ ! -d ".venv" ]]; then
+    echo "No virtual enviroment detected. Installing..."
+    python -m venv .venv
+fi
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install -e .
+
+echo "Virtual enviroment has been updated."
+echo "Log in to wandb..."
+wandb login
