375aa696c98a0b51621243e93905590cb2f32c59
Elin Skaanlund Berntsen <elinsberntsen@gmail.com>
2026-09-15 10:11:04 +0200

    feat(#2): add iteration to add all .h5 files
    The files are located in several different subdirectories,
    This enables loading of all the files, as well as resize/crop them for
    later

--- twod/dataloader/dataset_class.py
+++ twod/dataloader/dataset_class.py
@@ -29,15 +29,65 @@ class KeypointDataset(Dataset):
             filter (bool, optional): If True, removes images with keypoints (0,0,0,0).
         """
         path = os.getenv("DATASET_PATH")
-        with h5py.File(path, "r") as data:
-            images = data["frames"][()]
-            keypoints = data["annotations"][()]
-        self.images = images
-        self.keypoints = keypoints
-        self.transform = transform
-        self.preprocessing = preprocessing
-        self.device = device
-        self.model_type = model_type
+        total_files = 0
+        target_path = Path(path)
+        all_images = []
+        all_keypoints = []
+        skipped_files = []
+
+        for root, _, files in os.walk(target_path):
+            for filename in files:
+                if filename.endswith(".h5"):
+                    total_files += 1
+                    filepath = Path(root) / filename
+
+                    with h5py.File(filepath, "r") as f:
+                        required = {"frames", "annotations"}
+
+                        if not required.issubset(f.keys()):
+                            missing = required - set(f.keys())
+                            # Add incase some files were to miss some annotations
+                            print(
+                                f"SKIPPING: {filepath}\n"
+                                f"  Missing: {sorted(missing)}\n"
+                                f"  Available: {list(f.keys())}"
+                            )
+
+                            skipped_files.append((filepath, missing))
+                            continue
+                        frames = f["frames"][:]
+                        annotations = f["annotations"][:]
+
+                        if frames.ndim == 3 and frames.shape[0] != annotations.shape[0]:
+                            frames = frames.transpose(2, 0, 1)
+
+                        print("Frames before preprocessing:", frames.shape)
+                        print("Annotations:", annotations.shape)
+
+                        # It is expected that the images are 256,256, so they must be reshapes/cropped
+                        frames, annotations = resize_or_crop_image_np(
+                            frames,
+                            annotations,
+                            target_size=(256, 256),
+                        )
+
+                        all_images.append(frames)
+                        all_keypoints.append(annotations)
+
+        print(f"Found {total_files} .h5 files in path.")
+        print(f"Loaded {len(all_images)} .h5 files.")
+        print(f"Skipped {len(skipped_files)} .h5 files.")
+
+        if not all_images:
+            raise RuntimeError("No valid .h5 files were found.")
+
+        images = np.concatenate(all_images, axis=0)
+        keypoints = np.concatenate(all_keypoints, axis=0)
+
+        print(f"Total images: {len(images)}")
+        print(f"Images shape: {images.shape}")
+        print(f"Keypoints shape: {keypoints.shape}")
+
         if filter:
             # Finding unannotated keypoints
             unannotated_indices = np.where(np.all(keypoints == 0, axis=1))[0]
@@ -49,7 +99,11 @@ class KeypointDataset(Dataset):
             )[0]
 
             # Combining both cases
-            invalid_indices = np.unique(np.concatenate((unannotated_indices, out_of_bounds_indices)))
+            invalid_indices = np.unique(
+                np.concatenate((unannotated_indices, out_of_bounds_indices))
+            )
+
+            print(f"Removing {len(invalid_indices)} invalid samples.")
 
             images = np.delete(images, invalid_indices, axis=0)
             keypoints = np.delete(keypoints, invalid_indices, axis=0)
@@ -67,10 +121,10 @@ class KeypointDataset(Dataset):
     def __getitem__(self, idx):
         img = self.images[idx]
         if img.max() > 1:
-            img = img.astype(np.float32) / 255.0  
-        img = np.expand_dims(img, axis = 0)
+            img = img.astype(np.float32) / 255.0
+        img = np.expand_dims(img, axis=0)
 
-        img = preprocess_images(img, model_type = self.model_type, device=self.device)
+        img = preprocess_images(img, model_type=self.model_type, device=self.device)
 
         keypoint = self.keypoints[idx]
 
