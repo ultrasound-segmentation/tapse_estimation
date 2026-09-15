4cf1b893c9e9930615dceeb0c3ad7b2cdc071946
Elin Skånlund Berntsen <154683890+elinsbe@users.noreply.github.com>
2026-09-15 15:13:44 +0200

    2 fix the dataloading scipts to work on idun (#3)
    * feat: change hardcoded paths to use .env file
    
    * feat: add inital sbatch training script
    
    * fix(#2): update .gitignore and add to sbatch script
    
    * feat(#2): add iteration to add all .h5 files
    
    The files are located in several different subdirectories,
    This enables loading of all the files, as well as resize/crop them for
    later
    
    * docs(#2): update docs with .env instructions
    
    * fix(#2): add missing imports

--- twod/dataloader/dataset_class.py
+++ twod/dataloader/dataset_class.py
@@ -4,18 +4,37 @@ import numpy as np
 import os
 from torchvision import transforms as T
 from torchvision.transforms import v2 as T
+import h5py
+from dotenv import load_dotenv
+from pathlib import Path
 
-from twod.dataloader.preprocessing import preprocess_images
+load_dotenv()
+
+
+from twod.dataloader.preprocessing import (
+    preprocess_images,
+    resize_or_crop_image_np,
+)
 from twod.augmentations.img_augm import apply_transform
 from utils.plot import visualize_image
 
-# this file defines a custom PyTorch Dataset class called KeypointDataset for 
-#loading and preprocessing image-keypoint pairs from a NumPy .npz file. It's designed 
-#for keypoint detection, so it returns the images, coupled with the corresponding keypoints. 
+# this file defines a custom PyTorch Dataset class called KeypointDataset for
+# loading and preprocessing image-keypoint pairs from a NumPy .npz file. It's designed
+# for keypoint detection, so it returns the images, coupled with the corresponding keypoints.
 # It also performs image augmentations
 
+
 class KeypointDataset(Dataset):
-    def __init__(self, numpy_dataset, transform=None, filter=False, preprocessing= False, device='cpu', model_type = 'U-Net'):
+    def __init__(
+        self,
+        images=None,
+        keypoints=None,
+        transform=None,
+        filter=False,
+        preprocessing=False,
+        device="cpu",
+        model_type="U-Net",
+    ):
         """
         Args:
             images (list of np.array): List of grayscale images as numpy arrays.
@@ -23,23 +42,83 @@ class KeypointDataset(Dataset):
             transform (callable, optional): Image transformations.
             filter (bool, optional): If True, removes images with keypoints (0,0,0,0).
         """
+        path = os.getenv("DATASET_PATH")
+        total_files = 0
+        target_path = Path(path)
+        all_images = []
+        all_keypoints = []
+        skipped_files = []
 
-        file = np.load(numpy_dataset)
-        images = file['images']
-        keypoints = file['keypoints']
+        for root, _, files in os.walk(target_path):
+            for filename in files:
+                if filename.endswith(".h5"):
+                    total_files += 1
+                    filepath = Path(root) / filename
+
+                    with h5py.File(filepath, "r") as f:
+                        required = {"frames", "annotations"}
+
+                        if not required.issubset(f.keys()):
+                            missing = required - set(f.keys())
+                            # Add incase some files were to miss some annotations
+                            print(
+                                f"SKIPPING: {filepath}\n"
+                                f"  Missing: {sorted(missing)}\n"
+                                f"  Available: {list(f.keys())}"
+                            )
+
+                            skipped_files.append((filepath, missing))
+                            continue
+                        frames = f["frames"][:]
+                        annotations = f["annotations"][:]
+
+                        if frames.ndim == 3 and frames.shape[0] != annotations.shape[0]:
+                            frames = frames.transpose(2, 0, 1)
+
+                        print("Frames before preprocessing:", frames.shape)
+                        print("Annotations:", annotations.shape)
+
+                        # It is expected that the images are 256,256, so they must be reshapes/cropped
+                        frames, annotations = resize_or_crop_image_np(
+                            frames,
+                            annotations,
+                            target_size=(256, 256),
+                        )
+
+                        all_images.append(frames)
+                        all_keypoints.append(annotations)
+
+        print(f"Found {total_files} .h5 files in path.")
+        print(f"Loaded {len(all_images)} .h5 files.")
+        print(f"Skipped {len(skipped_files)} .h5 files.")
+
+        if not all_images:
+            raise RuntimeError("No valid .h5 files were found.")
+
+        images = np.concatenate(all_images, axis=0)
+        keypoints = np.concatenate(all_keypoints, axis=0)
+
+        print(f"Total images: {len(images)}")
+        print(f"Images shape: {images.shape}")
+        print(f"Keypoints shape: {keypoints.shape}")
 
         if filter:
-            # Finding unannotated keypoints
             unannotated_indices = np.where(np.all(keypoints == 0, axis=1))[0]
 
             # Finding out-of-bounds keypoints
             out_of_bounds_indices = np.where(
-                (keypoints[:, 0] < 0) | (keypoints[:, 0] > 256) |
-                (keypoints[:, 1] < 0) | (keypoints[:, 1] > 256)
+                (keypoints[:, 0] < 0)
+                | (keypoints[:, 0] > 256)
+                | (keypoints[:, 1] < 0)
+                | (keypoints[:, 1] > 256)
             )[0]
 
             # Combining both cases
-            invalid_indices = np.unique(np.concatenate((unannotated_indices, out_of_bounds_indices)))
+            invalid_indices = np.unique(
+                np.concatenate((unannotated_indices, out_of_bounds_indices))
+            )
+
+            print(f"Removing {len(invalid_indices)} invalid samples.")
 
             images = np.delete(images, invalid_indices, axis=0)
             keypoints = np.delete(keypoints, invalid_indices, axis=0)
@@ -57,10 +136,10 @@ class KeypointDataset(Dataset):
     def __getitem__(self, idx):
         img = self.images[idx]
         if img.max() > 1:
-            img = img.astype(np.float32) / 255.0  
-        img = np.expand_dims(img, axis = 0)
+            img = img.astype(np.float32) / 255.0
+        img = np.expand_dims(img, axis=0)
 
-        img = preprocess_images(img, model_type = self.model_type, device=self.device)
+        img = preprocess_images(img, model_type=self.model_type, device=self.device)
 
         keypoint = self.keypoints[idx]
 
@@ -77,11 +156,13 @@ class KeypointDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = 'D:/mmissana/data/dataset/train.npz'
-    keypoint_dataset = KeypointDataset(dataset, filter=True)
+    path = os.getenv("DATASET_PATH")
+    with h5py.File(path, "r") as data:
+        images = data["frames"][()]
+        keypoints = data["annotations"][()]
+    keypoint_dataset = KeypointDataset(images=images, keypoints=keypoints, filter=True)
+
     print(f"Number of images: {len(keypoint_dataset)}")
     print(f"Image shape: {keypoint_dataset[0][0].shape}")
     print(f"Keypoint shape: {keypoint_dataset[0][1].shape}")
     print(f"Keypoint coordinates: {keypoint_dataset[0][1]}")
-
-    
