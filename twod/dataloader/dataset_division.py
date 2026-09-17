73c7368b0e1e873382c2db3e8a1d66e70daf4e72
Sarah Norton <sbnorton@stud.ntnu>
2026-09-17 14:11:47 +0200

    fix: correct the hardcoded paths to env and throw errors

--- twod/dataloader/dataset_division.py
+++ twod/dataloader/dataset_division.py
@@ -2,133 +2,50 @@ import os
 import numpy as np
 import h5py
 import torch
-from dataloader.preprocessing import resize_or_crop_image_np
+from preprocessing import resize_or_crop_image_np
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
 
 # old script to create numpy dataset from files and txt file with the division
-
-
 # Function to divide dataset based on a text file containing train/val/test splits
 """
 The txtfile should be something like this:
 Validation:
-400001 
-660001
-730001  
-135001 
+name.h5
 Test:
-106001 
-157001 
-184001 
-661001 
-800001 
-920001 
-112001 
+another_name.h5
 Training:
-other
+etc.
 """
 
 
-def dataset_division_from_txt(
-    txt_path,
-    data_separated_by_video_path=r"D:\mmissana\data\dataset_separated_by_video_256",
-    save_path=r"D:\mmissana\data\dataset_256",
-):
-
-    # Create save directory if it doesn't exist
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    # Read the dataset division file
-    with open(txt_path, "r") as f:
-        lines = f.readlines()
-
-    # Lists to store video names for validation and test sets
-    test = []
-    val = []
-
-    # Flags to track which section is being read
-    flag_val = False
-    flag_test = False
-
-    for line in lines:
-        line = line.strip()  # Remove leading/trailing spaces and newline characters
-
-        if line == "Validation:":
-            flag_val = True
-            continue
-        if line == "Test:":
-            flag_test = True
-            flag_val = False
-            continue
-        if line == "Training:":
-            flag_val = False
-            flag_test = False
-            continue
-
-        # Add video names to respective lists
-        if flag_val:
-            val.append(line)
-        if flag_test:
-            test.append(line)
-
-    # Lists to store images and keypoints for each dataset split
-    img_val_list = []
-    img_test_list = []
-    img_train_list = []
-    keypoint_val_list = []
-    keypoint_test_list = []
-    keypoint_train_list = []
-
-    # Process each file in the dataset folder
-    for file in os.listdir(data_separated_by_video_path):
-        file_name = file.split(".")[0]  # Extract video name without extension
-        file_path = os.path.join(data_separated_by_video_path, file)
-
-        # Load the .npz file containing images and keypoints
-        data = np.load(file_path)
-
-        if file_name in val:  # If the file belongs to the validation set
-            for i in range(data["images"].shape[0]):
-                img_val_list.append(data["images"][i])
-                keypoint_val_list.append(data["keypoints"][i])
-        elif file_name in test:  # If the file belongs to the test set
-            for i in range(data["images"].shape[0]):
-                img_test_list.append(data["images"][i])
-                keypoint_test_list.append(data["keypoints"][i])
-        else:  # If the file belongs to the training set (default case)
-            for i in range(data["images"].shape[0]):
-                img_train_list.append(data["images"][i])
-                keypoint_train_list.append(data["keypoints"][i])
-
-    # Save the divided datasets into compressed .npz files
-    np.savez_compressed(
-        os.path.join(save_path, "val.npz"),
-        images=np.array(img_val_list),
-        keypoints=np.array(keypoint_val_list),
-    )
-    np.savez_compressed(
-        os.path.join(save_path, "test.npz"),
-        images=np.array(img_test_list),
-        keypoints=np.array(keypoint_test_list),
-    )
-    np.savez_compressed(
-        os.path.join(save_path, "train.npz"),
-        images=np.array(img_train_list),
-        keypoints=np.array(keypoint_train_list),
-    )
-
-
 def dataset_division_from_txt_h5(
-    txt_path, save_path=r"D:\mmissana\data\RV_PATIENTS\dataset_256"
+    txt_path, save_path=os.getenv("CONVERTED_DATASET_PATH", "")
 ):
+    data_path = Path(os.getenv("DATASET_PATH", ""))
 
     # Create save directory if it doesn't exist
     if not os.path.exists(save_path):
+        print(f"No directory detected. Creating directory {save_path}")
         os.makedirs(save_path)
 
+    if not os.path.isfile(txt_path):
+        raise FileNotFoundError(f"Division file not found: {txt_path}")
+
     # Read the dataset division file
-    with open(txt_path, "r") as f:
-        lines = f.readlines()
+    try:
+        with open(txt_path, "r") as f:
+            lines = f.readlines()
+            is_empty = 1 / len(lines)
+    except FileNotFoundError:
+        print("ERROR! File not found!")
+        raise FileNotFoundError
+    except ZeroDivisionError:
+        print(f"ERROR! {txt_path} is an empty file!")
+        # TODO: Add custom exceptions
+        raise FileExistsError
 
     # Lists to store video names for validation and test sets
     test = []
@@ -143,17 +60,17 @@ def dataset_division_from_txt_h5(
     for line in lines:
         line = line.strip()  # Remove leading/trailing spaces and newline characters
 
-        if line == "validation:":
+        if line == "Validation:":
             flag_val = True
             flag_test = False
             flag_train = False
             continue
-        if line == "test:":
+        if line == "Test:":
             flag_test = True
             flag_val = False
             flag_train = False
             continue
-        if line == "training:":
+        if line == "Training:":
             flag_val = False
             flag_test = False
             flag_train = True
@@ -166,7 +83,13 @@ def dataset_division_from_txt_h5(
             test.append(line)
         if flag_train:
             training.append(line)
-
+    for name, split in [("training", training), ("val", val), ("test", test)]:
+        try:
+            1 / len(split)
+        except ZeroDivisionError:
+            print(f"ERROR! {name} is empty")
+            # TODO: Add custom exception
+            raise ZeroDivisionError
     # Lists to store images and keypoints for each dataset split
     img_val_list = []
     img_test_list = []
@@ -176,8 +99,21 @@ def dataset_division_from_txt_h5(
     keypoint_train_list = []
 
     for split in [training, val, test]:
-        for path in split:
-            print(path)
+        for relative_path in split:
+            relative_path = relative_path.strip()
+
+            # Allow absolute paths, such as /cluster/...
+            path = Path(relative_path)
+
+            # Otherwise interpret the path relative to DATASET_PATH
+            if not path.is_absolute():
+                path = data_path / path
+
+            if not path.is_file():
+                raise FileNotFoundError(f"H5 file not found: {path}")
+
+            print(f"Reading: {path}")
+
             with h5py.File(path, "r") as h5_file:
                 images = h5_file["frames"][()]
                 annotations = h5_file["annotations"][()]
@@ -234,5 +170,5 @@ def dataset_division_from_txt_h5(
 
 # Main execution
 if __name__ == "__main__":
-    txt_path = r"c:\Users\vcxr10\Desktop\dataset_division_by_patient.txt"  # Path to the dataset division text file
+    txt_path = os.getenv("DIVISION_TXT_PATH")  # Path to the dataset division text file
     dataset_division_from_txt_h5(txt_path)  # Call the function to process the dataset
